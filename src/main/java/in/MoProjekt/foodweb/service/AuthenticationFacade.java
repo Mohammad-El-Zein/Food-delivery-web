bec22b527e09f207dd5f248cfe243da4837b73b1
Mohammad El Zein <mhmdzein70781311@gmail.com>
2025-12-07 13:34:09 +0100

    Initial Spring Boot foodweb commit

---
+++ src/main/java/in/MoProjekt/foodweb/service/AuthenticationFacade.java
@@ -0,0 +1,10 @@
+package in.MoProjekt.foodweb.service;
+import org.springframework.security.core.Authentication;
+
+
+public interface AuthenticationFacade {
+    
+    Authentication getAuthentication();
+
+
+}
